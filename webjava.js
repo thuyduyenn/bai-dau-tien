c77e53b438042dbb5cc30ce53a3f39f1008259b2
Lê Duyên <lethithuyduyen@MacBook-Air-cua-Le-6.local>
2023-04-21 18:41:44 +0700

    add speak javascript

---
+++ webjava.js
@@ -0,0 +1,6 @@
+// button = document.querySelector("button")
+// text = document.querySelector("textarea");
+// button.addEventListener("click",() =>{
+//       let utterance = new SpeechSynthesisUtterance(text.value)
+//       speechSynthesis.speak(utterance);
+// })
